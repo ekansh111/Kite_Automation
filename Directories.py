1098b9b1878b66930581ccf8443f61f1d039cef3
Ekansh Gowda <ekansh.n@gmail.com>
2024-11-13 09:37:01 +0530

    Added Directories for mean revert and intraday

--- Directories.py
+++ Directories.py
@@ -11,3 +11,13 @@ WriteOptionDetailsFile = 'C:/Users/ekans/OneDrive/Documents/inputs/option_detail
 WriteAllContractDet = 'C:/Users/ekans/OneDrive/Documents/inputs/TEXT_INSTRUMENTS.csv'
 
 WorkDirectory = 'C:/Users/ekans/OneDrive/Documents/inputs/'
+
+IntraDayDirectory = r"C:\Users\ekans\OneDrive\Documents\Trading\IntraDay_Stocks_Selector"
+Nifty500ConstituentList = r"C:\Users\ekans\OneDrive\Documents\Trading\IntraDay_Stocks_Selector\ind_nifty500list.csv"
+
+MeanReversionCharts = r"C:\Users\ekans\OneDrive\Documents\Trading\Scripts\Charts"
+MeanReversionPortfolioValue = r"C:\Users\ekans\OneDrive\Documents\Trading\Scripts\Charts\Portfolio Value"
+MeanReversionZScore = r"C:\Users\ekans\OneDrive\Documents\Trading\Scripts\Charts\z score"
+MeanReversionSpreadResiduals = r"C:\Users\ekans\OneDrive\Documents\Trading\Scripts\Charts\Spread and Residuals"
+MeanReversionClosingPrice = r"C:\Users\ekans\OneDrive\Documents\Trading\Scripts\Charts\Closing Prices"
+MeanReversionCointigrationHeatMap = r"C:\Users\ekans\OneDrive\Documents\Trading\Scripts\Charts\Cointegration Heatmaps"
